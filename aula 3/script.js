98a9982eec6454705bf534ae3cf3d2be161bfb56
IGOR SILVA SCHIMITZ <igor_s_schimitz@estudante.sesisenai.org.br>
2025-07-17 21:27:10 -0300

    aula 3 arrays

---
+++ aula 3/script.js
@@ -0,0 +1,49 @@
+// Atv 1 
+
+const amigos = ['Enzo', 'Henrique balela', 'Porsche', 'Grazi', 'Akssa']
+
+for(aluno of amigos){
+    // console.log(`Olá, ${aluno} Bem-vindo(a) à aula de JavaScript.`)
+}
+
+//Atv 2
+
+// Crie um array com 5 números inteiros.
+// Use um for para somar todos os números e mostrar o resultado final no
+// console.
+
+// const numeros = [10, 10, 10, 10, 10]
+// let soma = 0
+
+// for (numero of numeros){
+//     soma += numero
+// }
+
+// console.log(soma)
+
+
+//Atv 3
+
+// Crie um array com 5 notas de um aluno. Depois, use um laço de repetição
+// para somar as notas e, ao final, calcule e mostre a média.
+// Objetivo: Reforçar a ideia de percurso com for, acumulador e uso prático do
+// array em um cálculo real.
+
+// const notas = [8, 1, 10, 5, 7]
+// let soma = 0
+
+// for (nota of notas){
+//     soma += nota
+// }
+
+// let divisao = soma / notas.length
+
+// console.log(divisao)
+
+
+// Atv 4
+// Crie um exemplo prático que use os métodos de array aprendidos em aula
+// (como push, pop, shift, unshift, length, entre outros).
+// Use sua criatividade para montar uma situação em que esses métodos
+// façam sentido — pode ser uma lista de tarefas, carrinho de compras, nomes
+// de jogadores, etc.
