b763e2bdfae39dd6547ac6ac90f69667bec55e88
IGOR SILVA SCHIMITZ <igor_s_schimitz@estudante.sesisenai.org.br>
2025-08-11 21:40:00 -0300

    aula 7

---
+++ aula-7/server.js
@@ -0,0 +1,41 @@
+const http = require('http') // modulo interno do node
+const port = 3000 // porta onde o node/servidor ira rodar
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {"Content-type" : "text/plain"})
+    res.write("Ola, servidor de nidejs esta funcionando '-'") //corpo
+    res.end() //finalizar
+})
+
+server.listen(port, ()=>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
+
+
+// lendo arquivos
+
+const fs = require("fs") //importando modulo interno do node chamawndo File System
+
+// fs.readFile('dados.txt', 'utf-8', (err,data) =>{ // chamando o metodo para ler o arquivo
+//     if(err) //se der erro vai aparecer no console
+//         console.log('erro:', err)
+
+//     console.log(data)//mostra od dados do meu  arquivo 'dados.txt'
+// })
+
+//Escrevendo no arquivo
+
+// fs.writeFile('dados.txt', 'ola pessoal', (erro) =>{
+//     if(erro)
+//         console.log("erro" + erro)
+
+//     console.log('arquivo salvo')
+// })
+
+
+fs.appendFile("dados.txt", "isso é um cinema absoluto", (err)=>{
+    if(err)
+        console.log("erro" + err)
+
+    console.log('linha adicionada com sucesso')
+})
